30807b1b56e0dffec8a19398b3ae4c8acf3937aa
mystique09 <knightkrypto7@gmail.com>
2022-10-19 02:40:39 +0800

    add: session type

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,7 +2,22 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	// interface Locals {}
+	interface Locals {
+		session: {
+			token: string;
+			user: {
+				id: string;
+				email: string;
+				verified: boolean;
+				profile: {
+					id: string;
+					avatar?: string;
+					name: string;
+					userId: string;
+				}
+			}
+		}
+	}
 	// interface PageData {}
 	// interface Error {}
 	// interface Platform {}
