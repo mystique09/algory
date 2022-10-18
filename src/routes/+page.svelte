b87cfd8a2e3ed35e6d3facacd72b37ad3b072c36
mystique09 <knightkrypto7@gmail.com>
2022-10-19 02:41:24 +0800

    chore: initial code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,14 @@
-<h1 class="text-primary">Welcome to Algory</h1>
+<div class="hero">
+	<div class="hero-body">
+		<h1 class="text-blue-500 font-bold">Algory</h1>
+	</div>
+</div>
+
 
 <svelte:head>
-  <title>Algory</title>
-  <meta name="description" content="Algory, where you can ask for questions, and also help others." />
+	<title>Algory</title>
+	<meta
+		name="description"
+		content="Algory, where you can ask for questions, and also help others."
+	/>
 </svelte:head>
