f0dea0c8341e1148910461c20742a53e36a11fbc
mystique09 <knightkrypto7@gmail.com>
2022-10-19 14:18:57 +0800

    add: poppins

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Navbar from '$lib/components/navbar.svelte';
+	import '@fontsource/poppins';
 	import '../app.postcss';
 	// import EventSource from 'eventsource';
 	// global.EventSource = EventSource;
