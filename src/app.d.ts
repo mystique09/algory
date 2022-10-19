02cfd3e04b93fa83ccc81788b3fe6ac216068add
mystique09 <knightkrypto7@gmail.com>
2022-10-19 16:01:54 +0800

    add: session type in locals

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,23 +2,23 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	interface Locals {
-		session: {
-			token: string;
-			user: {
-				id: string;
-				email: string;
-				verified: boolean;
-				profile: {
-					id: string;
-					avatar?: string;
-					name: string;
-					userId: string;
-				}
-			}
-		}
-	}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
+    interface Locals {
+        session: {
+            token: string;
+            user: {
+                id: string;
+                email: string;
+                verified: boolean;
+                profile: {
+                    id: string;
+                    avatar?: string;
+                    name: string;
+                    userId: string;
+                }
+            },
+        },
+    }
+    // interface PageData {}
+    // interface Error {}
+    // interface Platform {}
 }
