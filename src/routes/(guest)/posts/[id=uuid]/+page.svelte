b5c02eceecc5dce721b72c086ddbe0bb1160f57b
mystique09 <knightkrypto7@gmail.com>
2022-10-19 14:18:15 +0800

    add: posts route in (guest) group

---
+++ src/routes/(guest)/posts/[id=uuid]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { PageData } from '.svelte-kit/types/src/routes/$types';
+
+	export let data: PageData;
+</script>
+
+<h1>{data.title}</h1>
+<h1>{data.description}</h1>
