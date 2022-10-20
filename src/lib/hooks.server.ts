ea77e7742d0d6c65c3ab686984da11c80d8b0cd0
mystique09 <knightkrypto7@gmail.com>
2022-10-21 01:40:05 +0800

    fix: app won't detect session. chore: move to src, misplaced

--- src/lib/hooks.server.ts
+++
@@ -1,9 +0,0 @@
-import type { Handle } from "@sveltejs/kit";
-
-export const handle: Handle = async ({ event, resolve }) => {
-    const cookies = event.cookies.get("session") || "";
-    event.locals.session = JSON.parse(cookies);
-
-    const response = await resolve(event);
-    return response;
-}
