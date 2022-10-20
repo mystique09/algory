dbddac1e2db91f4f0f34414cc3631d905abebd4b
mystique09 <knightkrypto7@gmail.com>
2022-10-21 01:40:35 +0800

    feat: redirect user if not loggedin

---
+++ src/routes/(auth)/profile/+page.server.ts
@@ -0,0 +1,14 @@
+import type { PageServerLoad } from ".svelte-kit/types/src/routes/(auth)/profile/$types";
+import { redirect } from "@sveltejs/kit";
+
+export const load: PageServerLoad = async ({ parent }) => {
+  const { session } = await parent();
+  
+  if (!session.user) {
+    throw redirect(307, '/sign-in');
+  }
+  
+  return {
+    profile: session.user.profile
+  }
+}
