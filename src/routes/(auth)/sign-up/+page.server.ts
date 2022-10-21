f9b6bc987853c3c6e8378bbe0c9d792af5bc2591
mystique09 <knightkrypto7@gmail.com>
2022-10-22 01:47:36 +0800

    add: signup form actions

--- src/routes/(auth)/sign-up/+page.server.ts
+++ src/routes/(auth)/sign-up/+page.server.ts
@@ -1,5 +1,6 @@
+import { pbClient } from "$lib/db/pocketbase";
 import type { PageServerLoad } from ".svelte-kit/types/src/routes/(auth)/sign-in/$types";
-import { redirect } from "@sveltejs/kit";
+import { invalid, redirect, type Actions } from "@sveltejs/kit";
 
 export const load: PageServerLoad = async ({ parent }) => {
   const { session } = await parent();
@@ -9,4 +10,52 @@ export const load: PageServerLoad = async ({ parent }) => {
   }
 
   return {}
+}
+
+export const actions: Actions = {
+  async signup({ request }) {
+    const data = await request.formData();
+    const username = data.get("username");
+    const email = data.get("email");
+    const password = data.get("password");
+    const confirmPassword = data.get("confirm-password");
+
+    if (!username || !email || !password) {
+      return invalid(400, { credentials: true, message: "Missing required fields." });
+    }
+
+    if (password.length < 8) {
+      return invalid(400, { password, message: "Password must be atleast 8 characters." });
+    }
+
+    if (username.length < 8) {
+      return invalid(400, { username, message: "Username must be atleast 8 characters." });
+    }
+
+    if (password != confirmPassword) {
+      return invalid(400, { confirm: true, message: "Password mismatched." });
+    }
+
+    try {
+      const user = await pbClient.users.create({
+        email,
+        password,
+        passwordConfirm: confirmPassword
+      });
+
+      await pbClient.users.requestVerification(user.email);
+
+      return { success: true, message: "New user created." };
+    } catch (e: any) {
+      console.log(e)
+      const { data } = e.data;
+
+      if (data.email) {
+        console.log(data, data.email);
+        return invalid(400, { email: true, message: data.email.message });
+      }
+
+      return invalid(e.status, { success: false, message: e.message });
+    }
+  }
 }
