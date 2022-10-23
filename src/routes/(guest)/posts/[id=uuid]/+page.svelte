2d2d5b73a3711b2e0b4e13cb5704de49acba6756
mystique09 <knightkrypto7@gmail.com>
2022-10-23 23:51:17 +0800

    chore: moved

--- src/routes/(guest)/posts/[id=uuid]/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import type { PageData } from '.svelte-kit/types/src/routes/$types';
-
-	export let data: PageData;
-</script>
-
-<h1>{data.title}</h1>
-<h1>{data.description}</h1>
