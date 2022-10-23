148ccd15df966fd17822a8305f7e1adfae7bf2e9
mystique09 <knightkrypto7@gmail.com>
2022-10-24 01:04:42 +0800

    add: temporary questions layout

--- src/routes/(guest)/questions/+page.svelte
+++ src/routes/(guest)/questions/+page.svelte
@@ -1,1 +1,36 @@
-<h1>Posts</h1>
+<script>
+	import Question from '$lib/components/question.svelte';
+</script>
+
+<div class="container">
+	<div class="content px-4">
+		<div class="heading">
+			<h1>All questions</h1>
+			<a href="/questions/new">Ask question</a>
+		</div>
+		<div class="info mt-8">
+			<p>100 questions</p>
+		</div>
+		<div class="divider" />
+		<div class="questions">
+			<Question />
+			<Question />
+			<Question />
+			<Question />
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	.heading {
+		@apply flex items-center justify-between;
+	}
+
+	.heading > h1 {
+		@apply font-semibold text-xl;
+	}
+
+	.heading > a {
+		@apply px-8 py-4 bg-primary shadow-md text-secondary text-sm;
+	}
+</style>
