620c6909a7097837b0412832698bf2edb4dfe210
mystique09 <knightkrypto7@gmail.com>
2022-10-23 13:08:13 +0800

    feat: only show hero page if user is unauthenticated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,40 @@
-<div class="hero min-h-screen bg-base-120">
-	<div class="hero-content">
-		<div class="max-w-lg">
-			<h1 class="text-5xl font-bold text-primary">Algory</h1>
-			<p class="py-6 text-lg font-light">
-				Looking for a community to ask for help? You are in the right path! <span
-					class="text-lg font-semibold text-primary">Algory</span
-				> is like stackoverflow where you can ask for tech-related questions, answer questions, upvote
-				questions/answers everytime.
-			</p>
-			<a
-				href="/sign-up"
-				class="signup btn btn-md rounded-sm text-base bg-primary hover:bg-accent nt border-none"
-				>Ready? Click to Join!</a
-			>
-			<div class="py-8 flex flex-row max-w-md">
-				<a href="https://github.com/mystique09/algory">
-					<img class="h-12 w-12" src="/images/github.svg" alt="Github icon" />
-				</a>
-				<span class="text-sm">Visit repository</span>
+<script lang="ts">
+	import { page } from "$app/stores";
+
+
+</script>
+
+{#if $page.data.session.user}
+	<div>
+		<h1>{$page.data.session.user.email}</h1>
+		<p>TODO!: Show latest posts</p>
+	</div>
+{:else}
+	<div class="hero min-h-screen bg-base-120">
+		<div class="hero-content">
+			<div class="max-w-lg">
+				<h1 class="text-5xl font-bold text-primary">Algory</h1>
+				<p class="py-6 text-lg font-light">
+					Looking for a community to ask for help? You are in the right path! <span
+						class="text-lg font-semibold text-primary">Algory</span
+					> is like stackoverflow where you can ask for tech-related questions, answer questions, upvote
+					questions/answers everytime.
+				</p>
+				<a
+					href="/sign-up"
+					class="signup btn btn-md rounded-sm text-base bg-primary hover:bg-accent nt border-none"
+					>Ready? Click to Join!</a
+				>
+				<div class="py-8 flex flex-row max-w-md">
+					<a href="https://github.com/mystique09/algory">
+						<img class="h-12 w-12" src="/images/github.svg" alt="Github icon" />
+					</a>
+					<span class="text-sm">Visit repository</span>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
+{/if}
 
 <svelte:head>
 	<title>Algory</title>
