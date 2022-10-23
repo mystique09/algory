281d34ebbd7d14875ed9ec3331333ec9a4245fd0
mystique09 <knightkrypto7@gmail.com>
2022-10-23 23:51:08 +0800

    chore: separate to home component

---
+++ src/lib/components/home.svelte
@@ -0,0 +1,18 @@
+<div class="hero min-h-screen bg-base-120">
+	<div class="hero-content">
+		<div class="max-w-lg">
+			<h1 class="text-5xl font-bold text-primary">Algory</h1>
+			<p class="py-6 text-lg font-light">
+				Looking for a community to ask for help? You are in the right path! <span
+					class="text-lg font-semibold text-primary">Algory</span
+				> is like stackoverflow where you can ask for tech-related questions, answer questions, upvote
+				questions/answers everytime.
+			</p>
+			<a
+				href="/questions"
+				class="signup btn btn-sm rounded-full text-primary bg-white hover:bg-primary hover:text-white border-none ring ring-primary"
+				>View latest questions</a
+			>
+		</div>
+	</div>
+</div>
