f32aa1badbf33280209d47e3fc807bdefcdfb7bd
mystique09 <knightkrypto7@gmail.com>
2022-10-23 20:35:30 +0800

    feat: toast component

---
+++ src/lib/components/toast.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { toast, ToastType } from '$lib/stores/toast';
+	setTimeout(() => {
+		$toast = { type: ToastType.NOTHING, message: null };
+	}, 2000);
+</script>
+
+<div class="toast toast-top toast-end" class:hidden={!$toast.message}>
+	<div
+		class={`alert ${
+			$toast.type === 1
+				? 'alert-error'
+				: $toast.type === 2
+				? 'alert-success'
+				: $toast.type === 3
+				? 'alert-info'
+				: ''
+		}`}
+	>
+		<div><span>{$toast.message}</span></div>
+	</div>
+</div>
+
+<style lang="postcss">
+	.toast {
+		@apply absolute z-30;
+	}
+</style>
