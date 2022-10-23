363da7633aa15ef298b924cb20abb88d3501a5a5
mystique09 <knightkrypto7@gmail.com>
2022-10-24 00:06:02 +0800

    add: don' redirect if user is logged in

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,12 @@
-import type { PageServerLoad } from ".svelte-kit/types/src/routes/$types";
-import { redirect } from "@sveltejs/kit";
+// import type { PageServerLoad } from ".svelte-kit/types/src/routes/$types";
+// import { redirect } from "@sveltejs/kit";
 
-export const load: PageServerLoad = async ({ parent }) => {
-  const { session } = await parent();
+// export const load: PageServerLoad = async ({ parent }) => {
+//   const { session } = await parent();
   
-  if (!!session.user) {
-    throw redirect(307, '/explore');
-  }
+//   if (!!session.user) {
+//     throw redirect(307, '/explore');
+//   }
   
-  return {}
-}
+//   return {}
+// }
