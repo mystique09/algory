7c50c339cf331d1d7e50bb29bc5e7e730d9aa4d2
mystique09 <knightkrypto7@gmail.com>
2022-10-25 23:24:19 +0800

    feat: /post/:id page, and dummy comments

--- src/routes/(guest)/questions/[id=uuid]/+page.svelte
+++ src/routes/(guest)/questions/[id=uuid]/+page.svelte
@@ -1,17 +1,87 @@
 <script lang="ts">
-	import Question from '$lib/components/question.svelte';
-
+	import { page } from '$app/stores';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
-<a class="p-2 link link-primary" href="/questions">Go back</a>
-<Question
-	id={data.question.id}
-	author={data.question.author}
-	title={data.question.title}
-	description={data.question.description}
-	tags={data.question.tags}
-	views={data.question.views}
-/>
+<div class="container max-w-6xl">
+	<div class="wrap mt-24 mb-36">
+		<div class="flex items-center justify-end my-8">
+			<a class="btn btn-md btn-ghost bg-primary text-white" href="/questions/new">Ask question</a>
+		</div>
+		<h2 class="text-xl">{data.question.title}</h2>
+		<div class="info">
+			<p class="text-xs">
+				<span class="text-gray-500">Author </span><a
+					class="text-accent"
+					href={`/users/${data.question.author}`}>{data.question.author}</a
+				>
+			</p>
+			<p class="text-xs">
+				<span class="text-gray-500">Viewed </span>{data.question.views} times
+			</p>
+		</div>
+		<div class="divider" />
+		<div class="content">
+			<p>{data.question.description}</p>
+			<div class="tags mt-4">
+				{#each data.question.tags.split(', ') as tag}
+					<span class="text-xs p-1 bg-primary/20 text-primary mr-1">{tag}</span>
+				{/each}
+			</div>
+		</div>
+		<div class="answers mt-8 pl-8 max-w-lg">
+			<div class="answer">
+				<div class="divider" />
+				<p class="text-xs">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti et culpa tenetur
+					explicabo voluptatem? Modi accusamus voluptate facere, ducimus dolor error possimus
+					corrupti eligendi dolore nemo. At ipsa iure vitae!
+				</p>
+				<a href="/users/krypto" class="text-accent text-xs">krypto</a>
+			</div>
+			<div class="answer">
+				<div class="divider" />
+				<p class="text-xs">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti et culpa tenetur
+					explicabo voluptatem? Modi accusamus voluptate facere, ducimus dolor error possimus
+					corrupti eligendi dolore nemo. At ipsa iure vitae!
+				</p>
+				<a href="/users/krypto" class="text-accent text-xs">krypto</a>
+			</div>
+			<div class="answer">
+				<div class="divider" />
+				<p class="text-xs">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti et culpa tenetur
+					explicabo voluptatem? Modi accusamus voluptate facere, ducimus dolor error possimus
+					corrupti eligendi dolore nemo. At ipsa iure vitae!
+				</p>
+				<a href="/users/krypto" class="text-accent text-xs">krypto</a>
+			</div>
+		</div>
+		<div class="footer w-full mt-8">
+			<form class="w-full">
+				<label class="text-label" for="answer">Your answer</label>
+				<textarea
+					disabled={!$page.data.session.user}
+					id="answer"
+					name="answer"
+					class="textarea textarea-bordered h-48 w-full max-w-lg resize-none"
+					placeholder={`${!$page.data.session.user ? 'Sign in to answer' : 'Answer...'}`}
+				/>
+			</form>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	.container {
+		@apply px-4 mt-8 m-auto;
+	}
+
+	.info {
+		@apply mt-4;
+		@apply flex items-center justify-start gap-2;
+	}
+</style>
