e641b20d16f9e83ade479153a55714c43b64b558
mystique09 <knightkrypto7@gmail.com>
2022-10-25 23:47:06 +0800

    chore: remove font style

--- src/routes/(auth)/profile/+page.svelte
+++ src/routes/(auth)/profile/+page.svelte
@@ -2,4 +2,4 @@
 	import { page } from '$app/stores';
 </script>
 
-<h1 class="text-2xl font-bold">Welcome to your profile, {$page.data.email}!</h1>
+<h1>Welcome to your profile, {$page.data.email}!</h1>
