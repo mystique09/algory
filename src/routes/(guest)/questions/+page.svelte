d4f8b3749af9d2f5202bf0f430fe4e03dbb87a4f
mystique09 <knightkrypto7@gmail.com>
2022-10-26 00:18:08 +0800

    fix: use basic pagination for simplicity

--- src/routes/(guest)/questions/+page.svelte
+++ src/routes/(guest)/questions/+page.svelte
@@ -7,7 +7,7 @@
 
 	$: numPages = Number($page.url.searchParams.get('page')) || 1;
 	$: prevPage = numPages - 1 || numPages;
-	$: nextPage = numPages;
+	$: nextPage = numPages % data.questions.totalPages;
 </script>
 
 <div class="container mb-24">
@@ -16,8 +16,9 @@
 			<h1>All questions</h1>
 			<a href="/questions/new">Ask question</a>
 		</div>
-		<div class="info mt-8">
+		<div class="info mt-8 flex items-center justify-between">
 			<p>{data.questions.items.length} questions</p>
+			<span class="text-lg text-primary">Page {numPages} of {data.questions.totalPages}</span>
 		</div>
 		<div class="divider" />
 		<div class="questions flex flex-col items-center justify-center">
@@ -34,14 +35,8 @@
 		</div>
 	</div>
 	<div class="btn-group flex items-center justify-center m-4 gap-1">
-		{#if prevPage > 20}
-			<a href="/questions?page=1" class="btn">1</a>
-		{/if}
-		<a href={`/questions?page=${prevPage}`} class="btn">{prevPage}</a>
-		<a href={`/questions?page=${nextPage + 1}`} class="btn">{nextPage + 1}</a>
-		<a href="/questions?page=0" class="btn btn-disabled">...</a>
-		<a href="/questions?page=99" class="btn">99</a>
-		<a href="/questions?page=100" class="btn">100</a>
+		<a href={`/questions?page=${prevPage}`} class="btn">Previous</a>
+		<a href={`/questions?page=${nextPage + 1}`} class="btn">Next</a>
 	</div>
 </div>
 
