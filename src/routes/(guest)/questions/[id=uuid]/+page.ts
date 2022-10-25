1865e6d316034591ff4b999f1a3ec60f652cb53d
mystique09 <knightkrypto7@gmail.com>
2022-10-25 23:24:37 +0800

    fix: import types

--- src/routes/(guest)/questions/[id=uuid]/+page.ts
+++ src/routes/(guest)/questions/[id=uuid]/+page.ts
@@ -1,5 +1,5 @@
 import { pbClient } from "$lib/db/pocketbase";
-import type { PageLoad } from ".svelte-kit/types/src/routes/(guest)/questions/$types";
+import type { PageLoad } from "./$types";
 import { error } from "@sveltejs/kit";
 
 export const load: PageLoad = async ({ params }) => {
