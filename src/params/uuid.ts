25481e5f30676e512b579c9e5c1518fdf2fa41a1
mystique09 <knightkrypto7@gmail.com>
2022-10-25 12:12:31 +0800

    add: match postId(even though it's not a uuid

--- src/params/uuid.ts
+++ src/params/uuid.ts
@@ -1,5 +1,5 @@
 import type { ParamMatcher } from "@sveltejs/kit";
 
 export const match: ParamMatcher = (param) => {
-    return /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/g.test(param);
+    return /^[0-9a-zA-Z]{15}$/g.test(param);
 }
