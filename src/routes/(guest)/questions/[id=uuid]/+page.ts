0002957da4e1ddeb1b0cf23e02a78d65e790f017
mystique09 <knightkrypto7@gmail.com>
2022-10-28 03:21:05 +0800

    feat: include the comments, upvotes, and downvotes relationship for posts

--- src/routes/(guest)/questions/[id=uuid]/+page.ts
+++ src/routes/(guest)/questions/[id=uuid]/+page.ts
@@ -1,12 +1,16 @@
-import { pbClient } from "$lib/db/pocketbase";
 import type { PageLoad } from "./$types";
 import { error } from "@sveltejs/kit";
 
-export const load: PageLoad = async ({ params }) => {
+export const load: PageLoad = async ({ params, fetch }) => {
     try {
-        const question = await pbClient.records.getOne('posts', params.id, {
-            expand: 'comments'
+        const res = await fetch(`/api/questions/${params.id}`, {
+            method: "GET",
+            headers: {
+                'content-type': 'application/json'
+            }
         });
+        const question = await res.json();
+
         return { question };
     } catch (e: any) {
         throw error(404, 'Question not found!');
