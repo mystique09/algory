43c2b660a81602f3869e0948550325124f13e6a4
mystique09 <knightkrypto7@gmail.com>
2022-10-31 15:35:49 +0800

    add: upvote utility function

--- src/lib/utils/upvote.ts
+++ src/lib/utils/upvote.ts
@@ -1,11 +1,11 @@
 export async function voteBaseHandler(record: string, type: string, body: { user: string, record_id: string }) {
-    const res = await fetch(`/api/upvotes/${body.record_id}?record=${record}&type=${type}`, {
+    const res = await fetch(`/api/votes/${body.record_id}?record=${record}&type=${type}`, {
         method: 'POST',
         headers: {
             'content-type': 'application/json'
         },
         body: JSON.stringify({
-            user: body.user
+            user: body.user,
         })
     });
     const data = await res.json();
