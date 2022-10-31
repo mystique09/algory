4204f93a3f59506c871b900c0f4c7d02edc64b96
mystique09 <knightkrypto7@gmail.com>
2022-10-31 15:31:59 +0800

    feat: view user page

--- src/routes/(guest)/users/[id=uuid]/+page.svelte
+++ src/routes/(guest)/users/[id=uuid]/+page.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
-	let isFollowing: boolean = false;
-	let followers = 200;
-	let following = 0;
+	import { page } from '$app/stores';
+	import Question from '$lib/components/question.svelte';
+
+	import type { PageData } from './$types';
+	export let data: PageData;
+
+	let isFollowing = data.authenticated
+		? data.followers.some((u: typeof data.followers) => u.user === $page.data.user.id)
+		: false;
+	let followers = data.followers.length || 0;
+	let following = data.following.length || 0;
 
 	function followUser() {
 		isFollowing = !isFollowing;
@@ -10,7 +18,7 @@
 </script>
 
 <svelte:head>
-	<title>Mystique09's profile</title>
+	<title>{data.info.email}'s profile</title>
 	<meta
 		type="description"
 		content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora commodi inventore ipsum
@@ -29,19 +37,31 @@
 			<div class="info mt-2">
 				<div class="top flex items-center justify-between">
 					<div class="profile">
-						<h1 class="text-xl text-bold">Mystique09</h1>
+						<h1 class="text-lg text-bold">{data.info.email}</h1>
 						<p class="text-xs">{followers} Followers</p>
 						<p class="text-xs">{following} Following</p>
 					</div>
-					<div class="profile-actions">
-						<button type="button" on:click={followUser} class="btn btn-ghost w-14 h-14">
-							<img
-								src={`${isFollowing ? '/svgs/user-minus-solid.svg' : '/svgs/user-plus-solid.svg'}`}
-								alt="follow icon"
-							/>
-							<span class="text-xs">{isFollowing ? 'Unfollow' : 'Follow'}</span>
-						</button>
-					</div>
+					{#if data.authenticated}
+						{#if $page.data.user.id !== data.info.id}
+							<div class="profile-actions">
+								<button
+									type="button"
+									on:click={followUser}
+									class="btn btn-ghost h-12 flex items-center"
+								>
+									<div class="w-5 h-5">
+										<img
+											src={`${
+												isFollowing ? '/svgs/user-minus-solid.svg' : '/svgs/user-plus-solid.svg'
+											}`}
+											alt="follow icon"
+											class="w-full h-full"
+										/>
+									</div>
+								</button>
+							</div>
+						{/if}
+					{/if}
 				</div>
 				<div class="bottom mt-2">
 					<p class="text-xs font-light">
@@ -50,6 +70,19 @@
 					</p>
 				</div>
 				<div class="divider" />
+				<div class="questions flex flex-col items-center justify-center">
+					<h1>Activities</h1>
+					{#each data.questions as question}
+						<Question
+							id={question.id}
+							author={question.author}
+							title={question.title}
+							description={question.description}
+							tags={question.tags}
+							views={question.views}
+						/>
+					{/each}
+				</div>
 			</div>
 		</div>
 	</div>
@@ -57,7 +90,7 @@
 
 <style lang="postcss">
 	.container {
-		@apply h-full w-full;
+		@apply h-full w-full mb-24;
 	}
 
 	.wrap {
