98b9aeb53249d5f96d4fd3ababfac83722e66b81
mystique09 <knightkrypto7@gmail.com>
2022-10-31 15:34:13 +0800

    feat: render all questions from load

--- src/routes/(guest)/questions/+page.svelte
+++ src/routes/(guest)/questions/+page.svelte
@@ -3,6 +3,7 @@
 
 	import Question from '$lib/components/question.svelte';
 	import type { PageData } from './$types';
+
 	export let data: PageData;
 
 	$: numPages = Number($page.url.searchParams.get('page')) || 1;
@@ -10,46 +11,29 @@
 	$: nextPage = numPages % data.questions.totalPages;
 </script>
 
-<div class="container mb-24">
-	<div class="content px-4">
-		<div class="heading">
-			<h1>All questions</h1>
-			<a href="/questions/new">Ask question</a>
-		</div>
-		<div class="info mt-8 flex items-center justify-between">
-			<p>{data.questions.items.length} questions</p>
-			<span class="text-lg text-primary">Page {numPages} of {data.questions.totalPages}</span>
-		</div>
-		<div class="divider" />
-		<div class="questions flex flex-col items-center justify-center">
-			{#each data.questions.items as question}
-				<Question
-					id={question.id}
-					author={question.author}
-					title={question.title}
-					description={question.description}
-					tags={question.tags}
-					views={question.views}
-				/>
-			{/each}
-		</div>
-	</div>
-	<div class="btn-group flex items-center justify-center m-4 gap-1">
-		<a href={`/questions?page=${prevPage}`} class="btn">Previous</a>
-		<a href={`/questions?page=${nextPage + 1}`} class="btn">Next</a>
-	</div>
+<div class="info mt-8 flex items-center justify-between">
+	<p>{$page.data.questions.items.length} questions</p>
+	<span class="text-lg text-primary">Page {numPages} of {data.questions.totalPages}</span>
+</div>
+<div class="divider" />
+<div class="questions flex flex-col items-center justify-center">
+	{#each data.questions.items as question}
+		<Question
+			id={question.id}
+			author={question.author}
+			title={question.title}
+			description={question.description}
+			tags={question.tags}
+			views={question.views}
+		/>
+	{/each}
 </div>
 
-<style lang="postcss">
-	.heading {
-		@apply flex items-center justify-between;
-	}
-
-	.heading > h1 {
-		@apply font-semibold text-xl;
-	}
-
-	.heading > a {
-		@apply px-8 py-4 bg-primary shadow-md text-secondary text-sm;
-	}
-</style>
+<div class="btn-group flex items-center justify-center m-4 gap-1">
+	<a disabled={prevPage === 0} href={`/questions?page=${prevPage}`} class="btn btn-wide text-xs"
+		>Previous</a
+	>
+	<a disabled={nextPage === numPages} href={`/questions?page=${nextPage + 1}`} class="btn text-xs"
+		>Next</a
+	>
+</div>
