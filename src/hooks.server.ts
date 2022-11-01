300b2ec33e510d387a38eafbe67d6b1c7372857f
mystique09 <knightkrypto7@gmail.com>
2022-11-01 08:14:09 +0800

    feat: use the cookie returned from pockerbase, set it to session

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,19 @@
+import { BACKEND_URL } from "$env/static/private";
 import type { Handle } from "@sveltejs/kit";
+import PocketBase from 'pocketbase';
 
 export const handle: Handle = async ({ event, resolve }) => {
-    const cookies = event.cookies.get("session") || "{}";
-    event.locals.session = JSON.parse(cookies);
+    event.locals.pb = new PocketBase(BACKEND_URL || "http://localhost:8090");
+    event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || "");
+
+    if (event.locals.pb.authStore.isValid) {
+        event.locals.session = event.locals.pb.authStore.model;
+    } else {
+        event.locals.session = null;
+        event.locals.pb.authStore.clear();
+    }
 
     const response = await resolve(event);
+    response.headers.set('set-cookie', event.locals.pb.authStore.exportToCookie({ secure: process.env.NODE_ENV === "production" }))
     return response;
 }
