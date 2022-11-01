5040cd8549da1249b493b62cb29ac6253820dd13
mystique09 <knightkrypto7@gmail.com>
2022-11-01 08:11:20 +0800

    feat: load user profile instead

--- src/routes/(guest)/users/[id=uuid]/+page.server.ts
+++ src/routes/(guest)/users/[id=uuid]/+page.server.ts
@@ -1,25 +1,26 @@
-import { authAdmin, pbClient } from "$lib/db/pocketbase";
+import { parseNonPOJO } from "$lib/utils/helpers";
 import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params, locals }) => {
     const { id } = params;
     try {
-        await authAdmin();
+        const info = await locals.pb.records.getOne('profiles', id).then(parseNonPOJO);
+        const followers = await locals.pb.records.getFullList('followers', 2048, {
+            filter: `followed_user = "${info.userId}"`
+        }).then(parseNonPOJO);
 
-        const info = await pbClient.users.getOne(id, undefined).then(JSON.stringify).then(JSON.parse);
-        const followers = await pbClient.records.getFullList('followers', 2048, {
-            filter: `followed_user = "${id}"`
-        }).then(JSON.stringify).then(JSON.parse);
-        const following = await pbClient.records.getFullList('followers', 2048, {
-            filter: `user = "${id}"`
-        }).then(JSON.stringify).then(JSON.parse);
-        const questions = await pbClient.records.getFullList('posts', 2048, {
-            filter: `author = "${info.id}"`
-        }).then(JSON.stringify).then(JSON.parse);
+        const following = await locals.pb.records.getFullList('followers', 2048, {
+            filter: `user = "${info.userId}"`
+        }).then(parseNonPOJO);
+
+        const questions = await locals.pb.records.getFullList('posts', 2048, {
+            filter: `author = "${info.userId}"`
+        }).then(parseNonPOJO);
 
         return { info, followers, following, questions }
     } catch (e: any) {
+        console.log(e)
         if (e.status === 404) {
             throw error(e.status, 'User not found!');
         }
