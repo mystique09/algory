05f1fdd45dc322ee295bf67ff169d6b5e8a164c5
mystique09 <knightkrypto7@gmail.com>
2022-11-01 08:13:41 +0800

    chore: removed unused

--- src/lib/db/pocketbase.ts
+++
@@ -1,8 +0,0 @@
-import { ADMIN_PASSWORD, ADMIN_USERNAME, BACKEND_URL } from '$env/static/private';
-import PocketBase from 'pocketbase';
-
-export const pbClient = new PocketBase(BACKEND_URL || "http://localhost:8090");
-
-export const authAdmin = async () => {
-    await pbClient.admins.authViaEmail(ADMIN_USERNAME, ADMIN_PASSWORD);
-}
