4c084cdc87936f780608d4c870ac38d78456c05e
mystique09 <knightkrypto7@gmail.com>
2022-11-01 08:12:00 +0800

    chore: render appropriate data

--- src/routes/(guest)/questions/[id=uuid]/+page.server.ts
+++ src/routes/(guest)/questions/[id=uuid]/+page.server.ts
@@ -1,21 +1,20 @@
 import type { PageServerLoad } from "./$types";
 import { error } from "@sveltejs/kit";
-import { pbClient } from "$lib/db/pocketbase";
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params, locals }) => {
     try {
-        const question = await pbClient.records.getOne('posts', params.id, {
+        const question = await locals.pb.records.getOne('posts', params.id, {
             sort: '-created',
             filter: `id = "${params.id}"`,
             expand: 'comments,comments.upvotes,comments.downvotes'
         }).then(JSON.stringify).then(JSON.parse);
 
-        const upvotes = await pbClient.records.getFullList('post_upvotes', 2048, {
+        const upvotes = await locals.pb.records.getFullList('post_upvotes', 2048, {
             filter: `post_id = "${question.id}"`,
             sort: '-created'
         }).then(JSON.stringify).then(JSON.parse);
 
-        const downvotes = await pbClient.records.getFullList('post_downvotes', 2048, {
+        const downvotes = await locals.pb.records.getFullList('post_downvotes', 2048, {
             filter: `post_id = "${question.id}"`,
             sort: '-created'
         }).then(JSON.stringify).then(JSON.parse);
