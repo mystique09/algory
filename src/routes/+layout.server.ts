e06cb5c52de63214bcaa29e8ab0e00dac7f3bd9c
mystique09 <knightkrypto7@gmail.com>
2022-11-01 08:10:54 +0800

    feat: parse non pjo user

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,9 @@
+import { parseNonPOJO } from "$lib/utils/helpers";
 import type { LayoutServerLoad } from ".svelte-kit/types/src/routes/$types";
 
 export const load: LayoutServerLoad = async ({ locals }) => {
     return {
-        authenticated: !!locals.session.token,
-        user: locals.session.user
+        authenticated: !!locals.session,
+        user: parseNonPOJO(locals.session)
     };
 }
