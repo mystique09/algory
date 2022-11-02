d9719693589bb652f086a4c4e51f10d8489cd0ad
mystique09 <knightkrypto7@gmail.com>
2022-11-02 16:23:14 +0800

    fix: import the correct type

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,5 @@
 import { parseNonPOJO } from "$lib/utils/helpers";
-import type { LayoutServerLoad } from ".svelte-kit/types/src/routes/$types";
+import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async ({ locals }) => {
     return {
