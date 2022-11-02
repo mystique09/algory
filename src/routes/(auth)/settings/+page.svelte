4b66d87ddc460462e31d0e3e0de41dd7ef31e828
mystique09 <knightkrypto7@gmail.com>
2022-11-02 16:26:06 +0800

    feat: alert message if user is unverified

--- src/routes/(auth)/settings/+page.svelte
+++ src/routes/(auth)/settings/+page.svelte
@@ -2,7 +2,25 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-	console.log(data);
 </script>
 
-<h1>This is your settings {data.user.profile.name}</h1>
+<div class="container">
+	<div class="wrap">
+		{#if !data.user.verified}
+			<div class="alert alert-warning text-xs mb-2">Your email is not verified!</div>
+		{/if}
+		<h1>This is your settings {data.user.profile.name}</h1>
+	</div>
+</div>
+
+<style lang="postcss">
+	.container {
+		@apply bg-base-100;
+	}
+	.wrap {
+		@apply max-w-md m-auto;
+		@apply bg-white;
+		@apply h-auto;
+		@apply rounded-md;
+	}
+</style>
