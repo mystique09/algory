1fa131dbd225f8d50e6d7f9007550c69fe67464b
mystique09 <knightkrypto7@gmail.com>
2022-11-03 01:57:02 +0800

    chore: change font size of paragraph to 2xs, change button color

--- src/lib/components/home.svelte
+++ src/lib/components/home.svelte
@@ -1,18 +1,14 @@
-<div class="hero h-screen bg-base-120">
-	<div class="hero-content">
+<div class="hero h-screen bg-base-100 text-base-content">
+	<div class="hero-content h-full">
 		<div class="max-w-lg">
-			<h1 class="text-5xl font-bold text-primary">Algory</h1>
-			<p class="py-6 text-lg font-light">
+			<h1 class="text-5xl font-bold text-accent">Algory</h1>
+			<p class="pt-3 pb-4 text-xs font-light">
 				Looking for a community to ask for help? You are in the right path! <span
-					class="text-lg font-semibold text-primary">Algory</span
+					class="text-sm font-semibold text-secondary">Algory</span
 				> is like stackoverflow where you can ask for tech-related questions, answer questions, upvote
 				questions/answers everytime.
 			</p>
-			<a
-				href="/questions"
-				class="signup btn btn-sm rounded-full text-primary bg-white hover:bg-primary hover:text-white border-none ring ring-primary"
-				>View latest questions</a
-			>
+			<a href="/questions" class="btn btn-secondary">Latest questions</a>
 		</div>
 	</div>
 </div>
