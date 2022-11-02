c336fab058345a64507bc9e51fd0f796976865bb
mystique09 <knightkrypto7@gmail.com>
2022-11-02 16:24:43 +0800

    feat: update profiles and post's comments relation

--- src/routes/(guest)/questions/+page.server.ts
+++ src/routes/(guest)/questions/+page.server.ts
@@ -1,5 +1,6 @@
 import type { Actions, PageServerLoad } from "./$types";
-import { error, redirect } from "@sveltejs/kit";
+import { error, invalid, redirect } from "@sveltejs/kit";
+import { parseNonPOJO } from "$lib/utils/helpers";
 
 export const load: PageServerLoad = async ({ url: { searchParams }, locals }) => {
     const id = Number(searchParams.get('page')) || 1;
@@ -20,12 +21,12 @@ export const actions: Actions = {
         if (!locals.session) {
             throw redirect(303, '/questions');
         }
-
+        const session = parseNonPOJO(locals.session);
         const body = await request.formData();
         const title = body.get('title');
         const description = body.get('description');
         const tags = body.get('tags');
-        const author = locals.session?.id;
+        const author = session.profile.id;
 
         if (!title || !description || !author) {
             throw error(400, 'Missing required fields!');
@@ -36,14 +37,31 @@ export const actions: Actions = {
                 title: title.toString(),
                 description: description,
                 author,
-                tags: tags,
-                views: 0
+                tags: tags?.toString().split(', '),
+                views: 0,
+                comments: []
             };
 
             const newQuestion = await locals.pb.records.create('posts', data).then(JSON.stringify).then(JSON.parse);
+            const allQuestions = await locals.pb.records.getFullList('posts', 2048, {
+                filter: `author = "${author}"`
+            }).then(question => {
+                return question.map(question => question.id);
+            });
+            const allAnswers = await locals.pb.records.getFullList('comments', 2048, {
+                filter: `user = "${author}"`
+            }).then(answers => {
+                return answers.map(answer => answer.id);
+            });
+
+            await locals.pb.records.update('profiles', author, {
+                posts: allQuestions,
+                comments: allAnswers
+            });
+
             return { newQuestion }
         } catch (e: any) {
-            throw error(e.status, e.message);
+            return invalid(e.status, { failed: true, tags: e.data.data.tags.message });
         }
     }
 }
