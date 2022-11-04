1f6ca6b1058ec673b9e57bda020cc34ebfa8dbc4
mystique09 <knightkrypto7@gmail.com>
2022-11-05 02:24:51 +0800

    add: Answer component

---
+++ src/lib/components/answer/answer.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let author: string;
+	export let content: string;
+</script>
+
+<div class="answer">
+	<div class="divider" />
+	<a href={`/users/${author}`} class="text-accent text-xs">{author}</a>
+	<p class="text-xs">
+		{content}
+	</p>
+</div>
