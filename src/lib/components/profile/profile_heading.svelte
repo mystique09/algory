511a65d3f7ffec433fc88358a172a23fd1d3e85f
mystique09 <knightkrypto7@gmail.com>
2022-11-05 02:24:42 +0800

    add: Main component for profile

---
+++ src/lib/components/profile/profile_heading.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { PageData } from '.svelte-kit/types/src/routes/(guest)/users/[id=uuid]/$types';
+	import ProfileHeadingBanner from './profile_heading_banner.svelte';
+	import ProfileHeadingInfo from './profile_heading_info.svelte';
+	export let data: PageData;
+	export let followers: number;
+	export let following: number;
+	export let isFollowing: boolean;
+</script>
+
+<div class="heading">
+	<ProfileHeadingBanner
+		authenticated={data.authenticated}
+		userId={data.user?.id}
+		id={data.info.id}
+	/>
+	<ProfileHeadingInfo
+		{followers}
+		{following}
+		{isFollowing}
+		authenticated={data.authenticated}
+		name={data.info.name}
+		bio={data.info.bio}
+		userId={data.user?.id}
+		id={data.info.id}
+	/>
+</div>
+
+<style lang="postcss">
+	.heading {
+		@apply h-1/2;
+		@apply flex flex-col items-start;
+	}
+</style>
