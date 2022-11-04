af6b89df65577d28fe1829241078abd55e69c170
mystique09 <knightkrypto7@gmail.com>
2022-11-05 02:23:53 +0800

    add: QuestionForm component

---
+++ src/lib/components/question/question_form.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let authenticated: boolean;
+</script>
+
+<div class="form w-full mt-8">
+	<form class="w-full" method="POST" action="?/newComment">
+		<div class="form-control">
+			<label class="text-label" for="answer">Your answer</label>
+			<textarea
+				disabled={!authenticated}
+				id="answer"
+				name="answer"
+				class="textarea textarea-bordered textarea-accent h-48 w-full max-w-lg resize-none"
+				placeholder={`${!authenticated ? 'Sign in to answer' : 'Yada yada...'}`}
+			/>
+		</div>
+		{#if authenticated}
+			<button id="submit" class="mt-4 px-12 py-3 bg-accent text-accent-content"
+				>Submit answer</button
+			>
+		{/if}
+	</form>
+</div>
