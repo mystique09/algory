b954cc872b821dfe33a602b73dbf192071db4535
mystique09 <knightkrypto7@gmail.com>
2022-11-04 15:02:28 +0800

    chore: remove wrapper

--- src/routes/(auth)/sign-up/+page.svelte
+++ src/routes/(auth)/sign-up/+page.svelte
@@ -6,88 +6,71 @@
 	$toast = { type: form?.success ? ToastType.SUCCESS : ToastType.ERROR, message: form?.message };
 </script>
 
-<div class="main">
-	<div class="wrap">
-		<form method="POST" action="?/signup">
-			<div class="form-logo">
-				<img src="/images/algory.svg" alt="Algory logo" />
-			</div>
-			<div class="input-group">
-				<label for="username">Username</label>
-				<input
-					class="input input-normal input-bordered"
-					id="username"
-					name="username"
-					type="text"
-					placeholder="Username"
-					required
-				/>
-			</div>
-			<div class="input-group">
-				<label class:text-red-700={form?.credentials || form?.email} for="email">Email</label>
-				<input
-					class="input input-normal input-bordered"
-					id="email"
-					name="email"
-					type="email"
-					placeholder="Email"
-					required
-				/>
-			</div>
-			<div class="input-group">
-				<label for="password" class:text-red-700={form?.password || form?.credentials}
-					>Password</label
-				>
-				<input
-					class="input input-normal input-bordered"
-					id="password"
-					name="password"
-					type="password"
-					placeholder="Password"
-					required
-				/>
-			</div>
-			<div class="input-group">
-				<label for="confirm-password" class:text-red-700={form?.confirm || form?.credentials}
-					>Confirm password</label
-				>
-				<input
-					class="input input-normal input-bordered"
-					id="confirm-password"
-					name="confirm-password"
-					type="password"
-					placeholder="Confirm Password"
-					required
-				/>
-			</div>
-			<div class="flex items-center justify-center w-full mt-6">
-				<button
-					class="btn btn-md bg-primary border-none btn-block btn-wide text-white"
-					type="submit">Sign up</button
-				>
-			</div>
-			<p class="text-center text-sm mt-4">
-				Already registered? <a class="text-accent" href="/sign-in">Login</a>
-			</p>
-		</form>
+<form method="POST" action="?/signup">
+	<div class="form-logo">
+		<img src="/images/algory.svg" alt="Algory logo" />
 	</div>
-</div>
+	<div class="input-group">
+		<label for="username">Username</label>
+		<input
+			class="input input-normal input-bordered"
+			id="username"
+			name="username"
+			type="text"
+			placeholder="Username"
+			required
+		/>
+	</div>
+	<div class="input-group">
+		<label class:text-red-700={form?.credentials || form?.email} for="email">Email</label>
+		<input
+			class="input input-normal input-bordered"
+			id="email"
+			name="email"
+			type="email"
+			placeholder="Email"
+			required
+		/>
+	</div>
+	<div class="input-group">
+		<label for="password" class:text-red-700={form?.password || form?.credentials}>Password</label>
+		<input
+			class="input input-normal input-bordered"
+			id="password"
+			name="password"
+			type="password"
+			placeholder="Password"
+			required
+		/>
+	</div>
+	<div class="input-group">
+		<label for="confirm-password" class:text-red-700={form?.confirm || form?.credentials}
+			>Confirm password</label
+		>
+		<input
+			class="input input-normal input-bordered"
+			id="confirm-password"
+			name="confirm-password"
+			type="password"
+			placeholder="Confirm Password"
+			required
+		/>
+	</div>
+	<div class="flex items-center justify-center w-full mt-6">
+		<button
+			class="btn btn-md bg-accent border-none btn-block btn-wide text-accent-content"
+			type="submit">Sign up</button
+		>
+	</div>
+	<p class="text-center text-sm mt-4">
+		Already registered? <a class="text-secondary" href="/sign-in">Login</a>
+	</p>
+</form>
 
 <style lang="postcss">
-	.main {
-		@apply flex flex-col items-center;
-		@apply bg-base-100;
-	}
-
-	.wrap {
-		@apply max-w-md;
-		@apply bg-white;
-		@apply rounded-md;
-	}
-
 	form {
-		@apply px-12 py-12 m-auto;
-		@apply h-full;
+		@apply px-12 py-6;
+		@apply bg-white;
 		@apply shadow-md;
 	}
 
