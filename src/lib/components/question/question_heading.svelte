e609872380dfa4cf84ba1def39f433535112e92a
mystique09 <knightkrypto7@gmail.com>
2022-11-05 02:23:34 +0800

    add: QuestionHeading component

---
+++ src/lib/components/question/question_heading.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let title: string;
+	export let author: string;
+	export let views: number;
+</script>
+
+<div class="container mt-4">
+	<h2 class="text-2xl">{title}</h2>
+	<div class="info">
+		<p class="text-2xs">
+			<span class="text-gray-500">Author </span><a class="text-accent" href={`/users/${author}`}
+				>{author}</a
+			>
+		</p>
+		<p class="text-xs">
+			<span class="text-gray-500">Viewed </span>{views} times
+		</p>
+	</div>
+</div>
+
+<style lang="postcss">
+	.info {
+		@apply mt-4;
+		@apply flex items-center justify-start gap-2;
+	}
+</style>
