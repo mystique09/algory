9a120bf266264b135b23a318498e1616588e21e4
mystique09 <knightkrypto7@gmail.com>
2022-11-06 18:02:29 +0800

    add: alert component, remove alert after 2 seconds

--- src/lib/components/toast.svelte
+++ src/lib/components/toast.svelte
@@ -1,43 +1,21 @@
 <script lang="ts">
-	import { toast, ToastType } from '$lib/stores/toast';
-	import ErrorIcon from './icons/error_icon.svelte';
-	import InfoIcon from './icons/info_icon.svelte';
-	import SuccessIcon from './icons/success_icon.svelte';
-	import WarningIcon from './icons/warning_icon.svelte';
+	import { toast } from '$lib/stores/toast';
+	import Alert from './alert.svelte';
 
-	setTimeout(() => {
-		$toast = { type: ToastType.NOTHING, message: null };
-	}, 2000);
+	$: $toast.map((_, i) => {
+		setTimeout(() => {
+			$toast = $toast.filter((_, j) => i != j);
+		}, 2000);
+	});
 </script>
 
-{#if $toast.message}
-	<div class="toast toast-top toast-end" class:hidden={!$toast.message}>
-		<div
-			class={`alert ${
-				$toast.type === 1
-					? 'alert-error'
-					: $toast.type === 2
-					? 'alert-warning'
-					: $toast.type === 3
-					? 'alert-success'
-					: 'alert-info'
-			}`}
-		>
-			<div>
-				{#if $toast.type === 1}
-					<ErrorIcon />
-				{:else if $toast.type === 2}
-					<SuccessIcon />
-				{:else if $toast.type === 3}
-					<InfoIcon />
-				{:else if $toast.type === 4}
-					<WarningIcon />
-				{/if}
-				<span class="text-xs">{$toast.message}</span>
-			</div>
-		</div>
-	</div>
-{/if}
+<div class="absolute top-0 right-0 toast toast-top toast-end flex flex-col gap-2 h-auto">
+	{#if $toast.length > 0}
+		{#each $toast as alert}
+			<Alert type={alert.type} message={alert.message} />
+		{/each}
+	{/if}
+</div>
 
 <style lang="postcss">
 	.toast {
