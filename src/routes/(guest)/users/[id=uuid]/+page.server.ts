2d01a53131692ed631c35057b47c31c4470e7f11
mystique09 <knightkrypto7@gmail.com>
2022-11-07 05:07:38 +0800

    add: follow and unfollow actions(incase I will switch to this)

--- src/routes/(guest)/users/[id=uuid]/+page.server.ts
+++ src/routes/(guest)/users/[id=uuid]/+page.server.ts
@@ -50,5 +50,41 @@ export const actions: Actions = {
             }
             throw error(e.status, e.message);
         }
+    },
+
+    async followUser({ locals, params }) {
+        const { id: user } = params;
+        const { id: follower } = locals.session!;
+
+        try {
+            const data = {
+                hash: user + follower,
+                user,
+                follower
+            };
+
+            const newFollower = await locals.pb.collection('followers').create(data).then(parseNonPOJO);
+            return { success: true, message: 'User added to following!', newFollower };
+        } catch (e: any) {
+            if (e.status === 400 || e.status === 404) {
+                return invalid(e.status, { failed: true, message: 'Failed to follow user.' });
+            }
+            throw error(e.status, 'Something went wrong!');
+        }
+    },
+
+    async unfollowUser({ locals, request }) {
+        const data = await request.formData();
+        const followId = data.get('followId');
+
+        try {
+            await locals.pb.collection('followers').delete(followId?.toString()!);
+            return { success: true, message: 'User removed from following.' };
+        } catch (e: any) {
+            if (e.status === 400 || e.status === 404) {
+                return invalid(e.status, { failed: true, message: 'Failed to unfollow user!' });
+            }
+            throw error(e.status, 'Something went wrong!');
+        }
     }
 }
