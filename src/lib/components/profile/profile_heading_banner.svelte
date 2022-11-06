7309c42505efdc6fd921384845ba83f14b74f52d
mystique09 <knightkrypto7@gmail.com>
2022-11-06 18:03:36 +0800

    feat: isEditing mode

--- src/lib/components/profile/profile_heading_banner.svelte
+++ src/lib/components/profile/profile_heading_banner.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import EditIcon from '../icons/edit_icon.svelte';
-
+	import { isEditingMode } from '$lib/stores/profile';
 	export let userId: string;
 	export let id: string;
 	export let authenticated: boolean;
@@ -26,10 +26,25 @@
 	{#if authenticated}
 		{#if userId === id}
 			<button
+				on:click={() => ($isEditingMode = !$isEditingMode)}
 				type="button"
-				class="btn btn-ghost btn-square w-6 h-6 text-base-100 z-10 absolute top-0 right-2"
-				><EditIcon /></button
+				class="btn btn-ghost btn-square w-5 text-base-100 z-10 absolute top-0 right-2"
 			>
+				{#if !$isEditingMode}
+					<EditIcon />
+				{:else}
+					<svg
+						class="swap-on fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						width="32"
+						height="32"
+						viewBox="0 0 512 512"
+						><polygon
+							points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"
+						/></svg
+					>
+				{/if}
+			</button>
 		{/if}
 	{/if}
 </div>
