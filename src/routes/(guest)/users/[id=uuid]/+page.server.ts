50f0e78df8f244a43dbca30af20306ba6725e308
mystique09 <knightkrypto7@gmail.com>
2022-11-06 17:59:52 +0800

    feat: allow user to change profile

--- src/routes/(guest)/users/[id=uuid]/+page.server.ts
+++ src/routes/(guest)/users/[id=uuid]/+page.server.ts
@@ -1,6 +1,6 @@
 import { parseNonPOJO } from "$lib/utils/helpers";
-import { error } from "@sveltejs/kit";
-import type { PageServerLoad } from "./$types";
+import { error, invalid } from "@sveltejs/kit";
+import type { PageServerLoad, Actions } from "./$types";
 
 export const load: PageServerLoad = async ({ params, locals }) => {
     const { id } = params;
@@ -16,8 +16,7 @@ export const load: PageServerLoad = async ({ params, locals }) => {
         const following = await locals.pb.collection("followers").getFullList(2048, {
             filter: `follower.id = "${id}"`
         }).then(parseNonPOJO);
-
-        return { info, questions: info.questions, followers, following }
+        return { info, questions: info.questions, followers, following, followId: followers[0]?.id }
     } catch (e: any) {
         if (e.status === 404) {
             throw error(e.status, 'User not found!');
@@ -25,3 +24,31 @@ export const load: PageServerLoad = async ({ params, locals }) => {
         throw error(e.status, e.message);
     }
 };
+
+export const actions: Actions = {
+    async changeProfile({ locals, request }) {
+        const data = await request.formData();
+        const name = data.get('name');
+        const bio = data.get('bio');
+        const github = data.get('github');
+        const { id } = locals.session!;
+
+        try {
+            const data = {
+                name,
+                bio,
+                github
+            };
+
+            await locals.pb.collection('users').update(id, data).then(parseNonPOJO);
+
+            return { success: true, message: 'Profile saved.' };
+        } catch (e: any) {
+            console.log(e)
+            if (e.status === 400 || e.status === 404) {
+                return invalid(e.status, { failed: true, message: 'Failed to update profile' });
+            }
+            throw error(e.status, e.message);
+        }
+    }
+}
