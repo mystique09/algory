eb884ceba1da9602124d911f177095498c9dcb0d
mystique09 <knightkrypto7@gmail.com>
2022-11-06 17:59:19 +0800

    chore: separate alert component

---
+++ src/lib/components/alert.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { ToastType, toast } from '$lib/stores/toast';
+	import ErrorIcon from './icons/error_icon.svelte';
+	import InfoIcon from './icons/info_icon.svelte';
+	import SuccessIcon from './icons/success_icon.svelte';
+	import WarningIcon from './icons/warning_icon.svelte';
+
+	export let type: ToastType;
+	export let message: string | undefined | null;
+</script>
+
+<div
+	class={`alert ${
+		type === ToastType.ERROR
+			? 'alert-error'
+			: type === ToastType.WARNING
+			? 'warning'
+			: type === ToastType.SUCCESS
+			? 'alert-success'
+			: 'alert-info'
+	}`}
+>
+	<div>
+		{#if type === ToastType.ERROR}
+			<ErrorIcon />
+		{:else if type === ToastType.WARNING}
+			<WarningIcon />
+		{:else if type === ToastType.SUCCESS}
+			<SuccessIcon />
+		{:else if type === ToastType.INFO}
+			<InfoIcon />
+		{/if}
+		<span class="text-xs">{message}</span>
+	</div>
+</div>
