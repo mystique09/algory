504f6c077c02a8957b7524da0f5914ea1ffdf853
mystique09 <knightkrypto7@gmail.com>
2022-11-07 05:08:18 +0800

    feat: custom toast store

--- src/lib/stores/toast.ts
+++ src/lib/stores/toast.ts
@@ -1,4 +1,4 @@
-import { writable, type Writable } from 'svelte/store';
+import { derived, writable, type Readable, type Writable } from 'svelte/store';
 
 export enum ToastType {
     NOTHING,
@@ -9,18 +9,52 @@ export enum ToastType {
 }
 
 export type Toast = {
+    id: string;
     type: ToastType;
-    message: string | undefined | null;
+    message: string;
+    timeout: number;
 }
+
+const TIMEOUT = 2000;
 
 function newToast() {
-    const { set, update, subscribe }: Writable<Toast[]> = writable([]);
+    const _toast: Writable<Toast[]> = writable([]);
 
-    function addToast(type: ToastType, message: string | undefined) {
-        return update((toasts) => [...toasts, { type, message }]);
+    function addToast(message: string, type: ToastType = ToastType.INFO, timeout: number) {
+        _toast.update(state => {
+            return [...state, { id: randomId(), type, message, timeout }];
+        });
     }
 
-    return { set, update, subscribe, addToast };
+    const notifications: Readable<Toast[]> = derived(_toast, ($_toast: Toast[], set) => {
+        set($_toast);
+        if ($_toast.length > 0) {
+            const timer = setTimeout(() => {
+                _toast.update(state => {
+                    state.shift()
+                    return state
+                })
+            }, $_toast[0].timeout)
+            return () => {
+                clearTimeout(timer)
+            }
+        }
+    });
+
+    const { subscribe } = notifications;
+
+    return {
+        subscribe,
+        addToast,
+        warning: (message: string, timeout: number = TIMEOUT) => addToast(message, ToastType.WARNING, timeout),
+        error: (message: string, timeout: number = TIMEOUT) => addToast(message, ToastType.ERROR, timeout),
+        success: (message: string, timeout: number = TIMEOUT) => addToast(message, ToastType.SUCCESS, timeout),
+        info: (message: string, timeout: number = TIMEOUT) => addToast(message, ToastType.INFO, timeout),
+    }
 }
+
+function randomId() {
+    return '_' + Math.random().toString(36).substring(2, 6);
+};
 
 export const toast = newToast();
