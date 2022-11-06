171676aef38b23a7a911fed20a9da26925fa5038
mystique09 <knightkrypto7@gmail.com>
2022-11-07 05:07:20 +0800

    feat: success and error toast

--- src/routes/(guest)/users/[id=uuid]/+page.svelte
+++ src/routes/(guest)/users/[id=uuid]/+page.svelte
@@ -2,13 +2,26 @@
 	import ProfileActivities from '$lib/components/profile/profile_activities.svelte';
 	import ProfileHeading from '$lib/components/profile/profile_heading.svelte';
 	import type { PageData } from './$types';
+	import { toast } from '$lib/stores/toast';
+
 	export let data: PageData;
+
+	import type { ActionData } from './$types';
+	export let form: ActionData;
 
 	let isFollowing = data.authenticated
 		? data.followers.some((u: typeof data.followers) => u.follower === data.user.id)
 		: false;
 	let followers = data.followers.length || 0;
 	let following = data.following.length || 0;
+
+	if (form?.success) {
+		toast.success(form?.message);
+	}
+
+	if (form?.failed) {
+		toast.error(form?.message!, 5000);
+	}
 </script>
 
 <svelte:head>
