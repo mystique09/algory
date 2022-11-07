c941ad98cb27db6979d164bb87966175291e2d65
mystique09 <knightkrypto7@gmail.com>
2022-11-07 15:38:50 +0800

    feat: load all questions base on tag

---
+++ src/routes/(guest)/explore/[topic]/+page.server.ts
@@ -0,0 +1,18 @@
+import { parseNonPOJO } from "$lib/utils/helpers";
+import type { PageServerLoad } from ".svelte-kit/types/src/routes/(auth)/settings/$types";
+import { error } from "@sveltejs/kit";
+
+export const load: PageServerLoad = async ({ locals, params, url }) => {
+    const page = Number(url.searchParams.get('page')) || 0;
+    const { topic } = params;
+
+    try {
+        const questionWithTagsOf = await locals.pb.collection('questions').getList(page, 20, {
+            filter: `tags ~ "${topic}"`
+        }).then(parseNonPOJO);
+
+        return { topic: questionWithTagsOf }
+    } catch (e: any) {
+        throw error(e.status, 'Topic not found!');
+    }
+}
