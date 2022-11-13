a42ab7ddae1f0f3b4d3bacba08dc52d22e32d803
mystique09 <knightkrypto7@gmail.com>
2022-11-13 21:42:18 +0800

    chore: layout

--- src/lib/components/home.svelte
+++ src/lib/components/home.svelte
@@ -1,14 +1,19 @@
-<div class="hero h-screen bg-base-100 text-base-content">
+<script>
+</script>
+
+<div class="hero h-screen bg-none text-base-content">
 	<div class="hero-content h-full">
-		<div class="max-w-lg">
+		<div class="max-w-2xl md:flex md:flex-col md:items-start">
 			<h1 class="text-5xl font-bold text-accent">Algory</h1>
-			<p class="pt-3 pb-4 text-xs font-light">
+			<p class="pt-3 pb-4 text-xs md:text-lg font-light">
 				Looking for a community to ask for help? You are in the right path! <span
-					class="text-sm font-semibold text-secondary">Algory</span
+					class="md:text-2xl font-semibold text-secondary">Algory</span
 				> is like stackoverflow where you can ask for tech-related questions, answer questions, upvote
 				questions/answers everytime.
 			</p>
-			<a href="/questions" class="btn btn-secondary">Latest questions</a>
+			<a href="/questions" class="btn btn-secondary rounded-sm px-12 normal-case"
+				>Show latest questions</a
+			>
 		</div>
 	</div>
 </div>
