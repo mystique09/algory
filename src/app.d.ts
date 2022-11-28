609e733f3cd554e30e2a4acda3269c6a49971379
mystique09 <knightkrypto7@gmail.com>
2022-11-28 23:42:26 +0800

    add: types

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,15 +3,36 @@
 // and what to do when importing types
 
 declare namespace App {
-	interface Locals {
-		pb: import("pocketbase").default;
-		session: import("pocketbase").BaseModel | import("pocketbase").Admin | null;
-	}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
+    interface Locals {
+        pb: import("pocketbase").default;
+        session: import("pocketbase").BaseModel | import("pocketbase").Admin | null;
+    }
+    // interface PageData {}
+    // interface Error {}
+    // interface Platform {}
 }
 
+type UserInfoProp = {
+    userId: string;
+    infoId: string;
+    isAuthenticated: boolean;
+};
+
+type UserDataProp = {
+    followId: string;
+    name: string;
+    bio: string;
+    followers: number;
+    following: number;
+    isFollowing: boolean;
+    social: string;
+};
+
+type Answer = {
+    author: string;
+    content: string;
+};
+
 interface ImportMetaEnv {
-	VITE_BACKEND_URL: string;
+    VITE_BACKEND_URL: string;
 }
