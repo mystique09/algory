a07e029389bbc2e34298aceb57c59594038d32eb
mystique09 <knightkrypto7@gmail.com>
2022-12-05 00:30:55 +0800

    feat: nothing change

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,22 +3,22 @@ import type { Handle } from "@sveltejs/kit";
 import PocketBase from "pocketbase";
 
 export const handle: Handle = async ({ event, resolve }) => {
-	event.locals.pb = new PocketBase(BACKEND_URL || "http://localhost:8090");
-	event.locals.pb.authStore.loadFromCookie(event.request.headers.get("cookie") || "");
+    event.locals.pb = new PocketBase(BACKEND_URL || "http://localhost:8090");
+    event.locals.pb.authStore.loadFromCookie(event.request.headers.get("cookie") || "");
 
-	if (event.locals.pb.authStore.isValid) {
-		event.locals.session = event.locals.pb.authStore.model;
-	} else {
-		event.locals.session = null;
-		event.locals.pb.authStore.clear();
-	}
+    if (event.locals.pb.authStore.isValid) {
+        event.locals.session = event.locals.pb.authStore.model;
+    } else {
+        event.locals.session = null;
+        event.locals.pb.authStore.clear();
+    }
 
-	const response = await resolve(event);
-	response.headers.set(
-		"set-cookie",
-		event.locals.pb.authStore.exportToCookie({
-			secure: process.env.NODE_ENV === "production",
-		}),
-	);
-	return response;
+    const response = await resolve(event);
+    response.headers.set(
+        "set-cookie",
+        event.locals.pb.authStore.exportToCookie({
+            secure: process.env.NODE_ENV === "production",
+        }),
+    );
+    return response;
 };
